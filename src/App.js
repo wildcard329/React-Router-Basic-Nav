17df4775d1eff4b75c59a681d7b85353a224eef8
wildcard329 <enkiduwildman@gmail.com>
2020-02-24 13:03:15 -0800

    added components and routes in app

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
+import { Route } from 'react-router-dom';
 import { Home, About, Contact, Navigation } from './components';
 
 const App = () => (
   <div>
     <Navigation />
+    <Home exact path='/' component={Home} />
+    <About path='/about' component={About} />
+    <Contact path='/contact' component={Contact} />
   </div>
 );
 
 export default App;
+// Could have also done this via the Route component, but chose this method for neatness
